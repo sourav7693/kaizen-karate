dfb47118d01e3932517914b8574c1b6185dce86c
neelgit1010 <neel.binbrain1010@gmail.com>
2025-10-07 12:30:03 +0530

    update

--- src/components/home/GallerySection.tsx
+++ src/components/home/GallerySection.tsx
@@ -1,12 +1,14 @@
 export const dynamic = "force-dynamic";
 import { GalleryDocument } from "@/models/Gallery";
 import GalleryClient from "./GalleryClient";
+import Link from "next/link";
 
 const GallerySection = ({ Gallery }: { Gallery: GalleryDocument[] }) => {
   
   return (
-    <section className="flex flex-col justify-center items-center">
-      <h1 className="text-2xl font-bold text-defined-purple">Gallery</h1>
+    <section className="flex flex-col justify-center">
+      <h1 className="text-2xl font-bold text-defined-purple text-center">Gallery</h1>
+      <Link href="/media" className="text-defined-purple md:text-xl text-right hover:underline cursor-pointer">View All</Link>
       <GalleryClient Gallery={Gallery} />
     </section>
   );
